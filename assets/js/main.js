08cbd2e9e36001196a3ca99f670c773e1839b6e1
penta2himajin <penta2himajin@users.noreply.github.com>
2020-11-28 05:57:24 +0000

    deploy: d207736d58a18d2004f448c00fcd30f6917be46e

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,12 +55,12 @@
 
 	// Nav.
 		var $nav = $header.children('nav'),
-			$nav_li = $nav.find('li');
+			$nav_li = $nav[$nav.length - 1].find('li');
 
 		// Add "middle" alignment classes if we're dealing with an even number of items.
 			if ($nav_li.length % 2 == 0) {
 
-				$nav.addClass('use-middle');
+				$nav[$nav.length - 1].addClass('use-middle');
 				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
 
 			}
